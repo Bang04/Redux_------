4b2b0610a1318af5e24ed12d017b1089e39cb48c
younme20 <eunkyeong_b@naver.com>
2023-06-22 23:46:13 +0900

    feat :

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ import App from './App';
 import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//전체 애플리케이션에 리덕스 스토어를 제공하는 방법
 root.render(<Provider store = {store} ><App /></Provider>);
